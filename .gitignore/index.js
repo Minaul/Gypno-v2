658a95c93f6868e7527fe2e6caae03ce7dc497a5
Minaul <47003983+Minaul@users.noreply.github.com>
2019-02-16 19:44:13 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
-const bot = new Discord.Client();
-
+const bot = new Discord.Client()
+const superagent = require("superagent")
 var prefix = ">";
 
  bot.on('ready', function(){
@@ -9,8 +9,8 @@ var prefix = ">";
 
  const activities_list = [
     ">help", 
-    "Rick & Morty",
-    "Futurama",
+    "je suis codé par Minaul",
+    "le ciel ",
     ">help",
     ]; 
 
@@ -23,7 +23,7 @@ bot.on('ready', () => {
 
 
 
-bot.login(process.env.TOKEN);
+ bot.login(process.env.TOKEN);
 
 
 /*Message*/
@@ -31,7 +31,7 @@ bot.on('message', message => {
     if (message.content === prefix + "help"){ 
         var embed = new Discord.RichEmbed()
             .setTitle("Liste des Commandes")
-            .setDescription("Toute des commandes actuelle \n")
+            .setDescription("__**Infos :**__ \n")
             .addBlankField()
             .addField(">help","Affiche ce message.", true)
             .addField(">owner","Affiche le créateur du bot.", true)
@@ -43,8 +43,14 @@ bot.on('message', message => {
             .addField(">ban","Bannir un utilisateur", true)
             .addField(">clear","Suprimer des messages", true)
             .addBlankField()
+            .addField(":game_die: __**Divertissement**__ :game_die:","Toute les commandes de divertissement")
+            .addBlankField()
+            .addField(">chat","Affiche une photo de chat.", true)
+            .addField(">chien","Affiche une photo de chien.", true)
+            .addField(">meme","Affiche un meme (en anglais).", true)
+            .addBlankField()
             .setColor("0x#48ED6B")
-            .setFooter("[BETA] 𝔾𝕪𝕡𝕟𝕠 𝕧.𝟚 by Minaul")
+            .setFooter("𝔾𝕪𝕡𝕟𝕠 𝕧.𝟚 by Minaul")
         message.channel.send(embed);    
     }
 
@@ -101,7 +107,7 @@ bot.on('message',message =>{
        message.channel.send(":zap:**"+member.user.username + '** a été kick du serveur !')
     }
 });
- 
+
 /*Ban*/
 bot.on('message',message =>{
     if (!message.guild) return
@@ -149,6 +155,74 @@ bot.on('message', message => {
         .setColor("0x#FFFF00")
     message.channel.sendEmbed(embed)
 
-    }});
+}});
 
-    
+
+
+/*Image random*/
+bot.on('message', async message => {
+/*Chat*/
+    if (message.content === prefix + "chat") {
+        let msg = await message.channel.send("`Génération......`")
+
+        let {body} = await superagent
+        .get(`http://aws.random.cat/meow`)
+        //console.log(body.file)
+        if(!{body}) return message.channel.send("Je trouve pas d'image de chat :cry: réessayer !")
+
+           let cEmbed = new Discord.RichEmbed()
+           .setColor("0xd9890e")
+           .setDescription(`<:kawaii:546366855930183681> Voilà une image de chat.`, )
+           .setImage(body.file)
+           .setTimestamp()
+           .setFooter(`Demandé par ${message.author.username}`,bot.user.displayAvatarURL)
+
+           message.channel.send({embed : cEmbed})
+
+           msg.delete()
+  
+    }
+/*Chien*/
+    if (message.content === prefix + "chien") {
+        let msg = await message.channel.send("`Génération......`")
+
+        let {body} = await superagent
+        .get(`https://dog.ceo/api/breeds/image/random`)
+        //console.log(body.file)
+        if(!{body}) return message.channel.send("Je trouve pas d'image de chien :cry: réessayer !")
+
+           let dEmbed = new Discord.RichEmbed()
+           .setColor("0x7c7263")
+           .setDescription(`:dog: Voilà une image de chien.`, )
+           .setImage(body.message)
+           .setTimestamp()
+           .setFooter(`Demandé par ${message.author.username}`,bot.user.displayAvatarURL)
+
+           message.channel.send({embed : dEmbed})
+
+           msg.delete()
+  
+    }
+
+/*Meme*/
+    if (message.content === prefix + "meme") {
+        let msg = await message.channel.send("`Génération......`")
+
+        let {body} = await superagent
+        .get(`https://api-to.get-a.life/meme`)
+        //console.log(body.file)
+        if(!{body}) return message.channel.send("Je trouve pas de meme :cry: réessayer !")
+
+           let mEmbed = new Discord.RichEmbed()
+           .setColor("0x046118")
+           .setDescription(`<:pepeOK:538651918709293069> Voilà un meme !`, )
+           .setImage(body.url)
+           .setTimestamp()
+           .setFooter(`Demandé par ${message.author.username}`,bot.user.displayAvatarURL)
+
+           message.channel.send({embed : mEmbed})
+
+           msg.delete()
+    }
+});
+
